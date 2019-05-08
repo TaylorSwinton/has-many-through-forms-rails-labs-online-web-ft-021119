d64e1ea02bf598f367d50003e5c6e5e748fe75e3
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-08 12:36:16 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    binding.pry
+    @user = User.new(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
+
 end
